272d0eafc1b84f392edb0eec200669a12952e0ab
Henrique Bissoli Silva <emp.shad@gmail.com>
2016-10-06 15:38:39 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+(function(global){
+	"use strict";
+	
+	window.onerror	= function(what, line, file){
+		alert(what+'; '+line+'; '+file);
+	}
+	
+	document.addEventListener('deviceready', function(){
+		if(navigator.connection.type == 'none'){
+			alert("Sem conexão com a internet.");
+		}else{
+			var ref = window.open("http://www.litastore.com.br", "_self", 'location=no, hidden=yes');
+			ref.addEventListener('loadstart', function(){
+				document.getElementById("loading_div").className		= '';
+			});
+			ref.addEventListener('loadstop', function(){
+				setTimeout(function(){
+					document.getElementById("loading_div").className		= 'hideLoading';
+				}, 1000);
+			});
+		}
+	}, false);
+})(window);
